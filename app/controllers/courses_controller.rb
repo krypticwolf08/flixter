b9819894bdfd95046870109dfc0f9fc579493059
Nick Danley <danleynick@gmail.com>
2019-10-23 19:19:50 +0000

    Student course page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,14 @@
 class CoursesController < ApplicationController
+
   def index
     @courses = Course.all
+  end
+
+  def show
+    @courses = Course.find(params[:id])
+  end
+
+  def courses_params
+    params.require(:course).permit(:title, :cost, :description)
   end
 end
